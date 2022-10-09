df4a0f3aca2a31e42201ab33849dbf22c90b7d68
checkiamsiam <issiam02415@gmail.com>
2022-10-09 14:29:32 +0600

    intialize the app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
