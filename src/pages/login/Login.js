0e119d07cf121aefc49a793c8ea493283c847716
checkiamsiam <issiam02415@gmail.com>
2022-10-14 23:08:11 +0600

    error box designed

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -22,7 +22,7 @@ const Login = () => {
         <div className="md:w-1/2 w-full md:pb-20 pb-5">
           <div className="bg-bg-primary max-w-sm rounded-xl shadow-shadow-one shadow-lg p-5 mx-auto">
             <form onSubmit={handleSubmit(login)}>
-              <div className="mb-3">
+              <div className="mb-3 relative">
                 <input
                   className={`w-full px-3 py-3 rounded-lg  border-[1px] ${
                     errors.email ? "border-error focus:outline-error" : "border-dark-color-secondary focus:outline-blue-color"
@@ -31,16 +31,23 @@ const Login = () => {
                   name="email"
                   placeholder="Your email adress"
                   {...register("email", {
-                    required: { value: true, message: "enter your email" },
+                    required: { value: true, message: "Enter your email" },
                     pattern: {
                       value: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
-                      message: "please enter a valid email",
+                      message: "Please enter a valid email",
                     },
                   })}
                 />
-                {errors.email && <p className="text-sm text-error text-center">{errors.email.message}</p>}
+                {errors?.email && (
+                  <>
+                    <p className=" text-white text-center absolute w-full px-3 py-3 rounded-lg bg-error -top-14 ">{errors.email.message}</p>
+                    <div class="w-8 overflow-hidden inline-block absolute left-2 -top-2 ">
+                      <div class=" h-3 w-3 bg-error -rotate-45 transform origin-top-left"></div>
+                    </div>
+                  </>
+                )}
               </div>
-              <div className="mb-3">
+              <div className="mb-3 relative">
                 <input
                   className={`w-full px-3 py-3 rounded-lg  border-[1px] ${
                     errors.password ? "border-error focus:outline-error" : "border-dark-color-secondary focus:outline-blue-color"
@@ -49,11 +56,20 @@ const Login = () => {
                   name="password"
                   placeholder="Password"
                   {...register("password", {
-                    required: { value: true, message: "enter your password" },
-                    minLength: { value: 8, message: "password length is too short" },
+                    required: { value: true, message: "Enter your password" },
+                    minLength: { value: 8, message: "Password length is too short" },
                   })}
                 />
-                {errors.password && <p className="text-sm text-error text-center">{errors.password.message}</p>}
+                {errors?.password && (
+                  <>
+                    <p className=" text-white text-center md:absolute w-full px-3 py-3 rounded-lg bg-error md:-left-[103%]  top-0 md:mt-0 mt-2">
+                      {errors.password.message}
+                    </p>
+                    <div class="md:h-8 w-8 overflow-hidden inline-block absolute md:-left-3 md:top-2 top-12 left-2  ">
+                      <div class=" h-3 w-3 bg-error md:rotate-45 rotate-45 transform md:origin-top-left origin-bottom-left"></div>
+                    </div>
+                  </>
+                )}
               </div>
               <input
                 className="w-full bg-blue-color py-3 text-center rounded-lg cursor-pointer text-bg-primary font-bold text-xl hover:bg-[#166FE5]"
